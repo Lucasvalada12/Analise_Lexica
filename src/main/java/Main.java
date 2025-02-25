541e504192fdfd78cbed0ab19a5db3038d5290f8
Lucas Valadares <122353470+Lucasvalada12@users.noreply.github.com>
2025-02-25 23:58:39 +0000

    first

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,54 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int saldo = 0;
+        int num;
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+        do {
+            System.out.println("\nMenu:");
+            System.out.println("1. Visualizar saldo");
+            System.out.println("2. Depositar");
+            System.out.println("3. Sacar");
+            System.out.println("4. Transferir");
+            System.out.println("5. Sair");
+            System.out.print("Escolha uma opção: ");
+
+            num = input.nextInt();
+
+            if (num == 1) {
+                System.out.println("Saldo: " + saldo);
+            } else if (num == 2) {
+                System.out.print("Digite o valor a ser depositado: ");
+                int deposito = input.nextInt();
+                saldo += deposito;
+                System.out.println("Saldo atual: " + saldo);
+            } else if (num == 3) {
+                System.out.print("Digite o valor a ser sacado: ");
+                int saque = input.nextInt();
+                if (saque <= saldo) {
+                    saldo -= saque;
+                    System.out.println("Saldo atual: " + saldo);
+                } else {
+                    System.out.println("Saldo insuficiente!");
+                }
+            } else if (num == 4) {
+                System.out.print("Digite o valor a ser transferido: ");
+                int transferencia = input.nextInt();
+                if (transferencia <= saldo) {
+                    saldo -= transferencia;
+                    System.out.println("Saldo atual: " + saldo);
+                } else {
+                    System.out.println("Saldo insuficiente!");
+                }
+            } else if (num == 5) {
+                System.out.println("Saindo...");
+            } else {
+                System.out.println("Opção inválida!");
+            }
+        } while (num != 5);
+
+        input.close();
+    }
 }
